9eaf0f9c28308df7d12538db26f3fabe40665d04
rjablon03 <secrets_crasser.0l@icloud.com>
2025-02-07 11:04:53 -0500

    Familiarized myself with Typescript and installed/played around with Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import './App.css'
+import { useState } from 'react';
 
 function App() {
+  const name: string = "Ryan";
+  const [age, setAge] = useState<number>(22)
+
+  const increaseAge = (): void => {
+    setAge(age + 1)
+  }
+
+  const decreaseAge = (): void => {
+    setAge(age - 1)
+  }
+
   return (
-    <div>Hey!</div>
+    <div>
+      <h1 className='text-6xl'>Hey! My name is {name}!</h1>
+      <p className='font-bold'>I'm {age} years old!</p>
+      <button onClick={increaseAge}>Increase Age</button>
+      <button onClick={decreaseAge}>Decrease Age</button>
+    </div>
   )
 }
 
