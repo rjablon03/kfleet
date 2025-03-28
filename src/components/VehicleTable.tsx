c34a92836c2623975c2a2fc3229b601b45b3f718
rjablon03 <secrets_crasser.0l@icloud.com>
2025-03-28 16:18:16 -0400

    Delete functionality complete

--- src/components/VehicleTable.tsx
+++ src/components/VehicleTable.tsx
@@ -1,10 +1,11 @@
 import { db } from '../config/firebase'
-import { getDocs, collection } from 'firebase/firestore'
+import { getDocs, collection, doc, deleteDoc } from 'firebase/firestore'
 import { Vehicle } from '../models/Vehicle'
 import { useEffect, useState } from 'react';
 
 function VehicleTable() {
     const [vehicles, setVehicles] = useState<Vehicle[]>([]);
+    const [removeVehicles, setRemoveVehicles] = useState<string[]>([]);
 
     useEffect(() => {
         const fetchVehicles = async () => {
@@ -35,37 +36,68 @@ function VehicleTable() {
         fetchVehicles();
     }, []);
 
+    const prepRemoval = (event: React.ChangeEvent<HTMLInputElement>, vehicleId: string) => {
+        if (event.target.checked === true) {
+            setRemoveVehicles(prev => [...prev, vehicleId]);
+        }
+        else {
+            setRemoveVehicles(prev => prev.filter(id => id !== vehicleId))
+        }
+    }
+
+    const deleteVehicles = async () => {
+        if (removeVehicles.length > 0) {
+            try {
+                await Promise.all(
+                    removeVehicles.map(async (vehicleId) => {
+                        await deleteDoc(doc(db, 'vehicles', vehicleId));
+                    })
+                );
+                setVehicles(prev => prev.filter(vehicle => !removeVehicles.includes(vehicle.id)));
+                setRemoveVehicles([]);
+            } catch (error) {
+                console.error("Error deleting vehicles: ", error);
+            }
+        }
+    };
+
     return(
-        <table className="border-separate w-[95%] m-auto text-left border-spacing-y-1.5">
-            <thead>
-                <tr className="">
-                    <th></th>
-                    <th className="text-xl">Car Make</th>
-                    <th className="text-xl"> Car Model</th>
-                    <th className="text-xl">Year</th>
-                    <th className="text-xl">Body Style</th>
-                    <th className="text-xl">Mileage</th>
-                    <th className="text-xl">Fuel Type</th>
-                    <th className="text-xl">Needs Repair</th>
-                </tr>
-            </thead>
-            <tbody>
-                {vehicles.map((vehicle) => (
-                    <tr key={vehicle.id}>
-                        <td>
-                            <input type="checkbox" />
-                        </td>
-                        <td>{vehicle.make}</td>
-                        <td>{vehicle.model}</td>
-                        <td>{vehicle.year}</td>
-                        <td>{vehicle.bodyStyle}</td>
-                        <td>{vehicle.mileage}</td>
-                        <td>{vehicle.fuelType}</td>
-                        <td>{vehicle.needsRepair ? 'Yes' : 'No'}</td>
+        <>
+            <table className="border-separate w-[95%] m-auto text-left border-spacing-y-1.5">
+                <thead>
+                    <tr className="">
+                        <th></th>
+                        <th className="text-xl">Car Make</th>
+                        <th className="text-xl"> Car Model</th>
+                        <th className="text-xl">Year</th>
+                        <th className="text-xl">Body Style</th>
+                        <th className="text-xl">Mileage</th>
+                        <th className="text-xl">Fuel Type</th>
+                        <th className="text-xl">Needs Repair</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {vehicles.map((vehicle) => (
+                        <tr key={vehicle.id}>
+                            <td>
+                                <input
+                                    type="checkbox"
+                                    onChange={(event) => prepRemoval(event, vehicle.id)}
+                                />
+                            </td>
+                            <td>{vehicle.make}</td>
+                            <td>{vehicle.model}</td>
+                            <td>{vehicle.year}</td>
+                            <td>{vehicle.bodyStyle}</td>
+                            <td>{vehicle.mileage}</td>
+                            <td>{vehicle.fuelType}</td>
+                            <td>{vehicle.needsRepair ? 'Yes' : 'No'}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <button className="delete-vehicles bg-red-500 p-1 rounded-md text-white font-bold mt-4" onClick={deleteVehicles}>Delete Vehicles</button>
+        </>
     )
 }
 
