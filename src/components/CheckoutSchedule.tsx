3235d11f496bb0d7eca0321df1b7d2b1851be376
rjablon03 <secrets_crasser.0l@icloud.com>
2025-04-20 12:36:54 -0400

    Fixed vehicle schedule issue

--- src/components/CheckoutSchedule.tsx
+++ src/components/CheckoutSchedule.tsx
@@ -6,15 +6,15 @@ interface CheckoutData {
 }
 
 function CheckoutSchedule(props: CheckoutData) {
-    const checkouts = props.checkouts.filter((checkout) => !checkout.checkedIn)
-    if (checkouts.length === 0) {
+    const filteredCheckouts = props.checkouts.filter((checkout) => !checkout.checkedIn)
+    if (filteredCheckouts.length === 0) {
         return
     }
 
     return (
-        <div className={props.checkouts.length > 0 ? "bg-white ml-3 p-3 rounded-2xl shadow-2xl" : "hidden"}>
+        <div className={filteredCheckouts.length > 0 ? "bg-white ml-3 p-3 rounded-2xl shadow-2xl" : "hidden"}>
             <h1 className="text-2xl font-bold">Vehicle Schedule</h1>
-            {props.checkouts
+            {filteredCheckouts
                 .map((checkout) => (
                     <p key={checkout.id}>{`${format(checkout.startDate, "MMM d, yyyy h:mm a")} - ${format(checkout.endDate, "MMM d, yyyy h:mm a")}`}</p>
                 ))}
